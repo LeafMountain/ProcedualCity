e9e58640e01b6f9156c50e6b011f054166471574
leafmountain <lovbergalexander@gmail.com>
2019-09-13 13:45:54 +0200

    now using edges instead of the whole side

--- Assets/Scripts/MeshModuleGenerator.cs
+++ Assets/Scripts/MeshModuleGenerator.cs
@@ -8,15 +8,23 @@ public class MeshModuleGenerator : Generator
     public Mesh[] meshes = null;    // Take multiple meshes instead?
     public Vector3 sizePerTile = Vector3.one;
     public Material defaultMaterial = null;
+    public GameObject test;
 
     public bool debug = false;
 
     private void Start()
     {
-        if(debug)
+        MeshFilter[] filters = test.GetComponentsInChildren<MeshFilter>();
+        meshes = new Mesh[filters.Length];
+        for (int i = 0; i < filters.Length; i++)
         {
-            VisualDebug(GenerateTemplate());
+            meshes[i] = filters[i].sharedMesh;
         }
+
+        // if(debug)
+        // {
+        //     VisualDebug(GenerateTemplate());
+        // }
     }
 
     public override Module[] GenerateTemplate()
@@ -76,10 +84,16 @@ public class MeshModuleGenerator : Generator
         return Animator.StringToHash(combined);
     }
 
+    private bool OnEdge(Vector3 position)
+    {
+        Vector3 halfSize = sizePerTile * .5f;
+        return position.x == -halfSize.x || position.x == halfSize.x || position.y == -halfSize.y || position.y == halfSize.y || position.z == -halfSize.z || position.z == halfSize.z;
+    }
+
     private Module CreateModule(Mesh mesh)
     {
-        List<Vector3> verts = new List<Vector3>();
-        mesh.GetVertices(verts);
+        List<Vector3> verts = GetVertsAlogEdges(mesh);
+        // mesh.GetVertices(verts);
         verts = verts.Distinct().ToList();
         for (int i = 0; i < verts.Count; i++)
         {
@@ -90,8 +104,6 @@ public class MeshModuleGenerator : Generator
             verts[i] = vert;
         }
 
-        float allowedDiff = 0f;
-
         List<Vector3> left = new List<Vector3>();
         List<Vector3> right = new List<Vector3>();
         List<Vector3> back = new List<Vector3>();
@@ -99,19 +111,19 @@ public class MeshModuleGenerator : Generator
         List<Vector3> up = new List<Vector3>();
         List<Vector3> down = new List<Vector3>();      
 
-        Vector3 halfSizePerTile = sizePerTile * .5f;  
+        Vector3 halfSizePerTile = sizePerTile * .5f;
 
         for (int i = verts.Count - 1; i >= 0; i--)
         {
             // LEFT
-            if(verts[i].x == ( - halfSizePerTile.x))
+            if(verts[i].x == (-halfSizePerTile.x))
             {
                 Vector3 vert = verts[i];
                 vert.x = 0;
                 left.Add(vert);
             }
             // RIGHT
-            else if(verts[i].x == ( + halfSizePerTile.x))
+            else if(verts[i].x == (halfSizePerTile.x))
             {
                 Vector3 vert = verts[i];
                 vert.x = 0;
@@ -119,24 +131,22 @@ public class MeshModuleGenerator : Generator
             }
             
             // DOWN
-            if(verts[i].y == ( - halfSizePerTile.y))
+            if(verts[i].y == (-halfSizePerTile.y))
             {
                 Vector3 vert = verts[i];
                 vert.y = 0;
                 down.Add(vert);
             } 
             // UP
-            else if(verts[i].y == ( + halfSizePerTile.y))
+            else if(verts[i].y == (halfSizePerTile.y))
             {
                 Vector3 vert = verts[i];
                 vert.y = 0;
                 up.Add(vert);
             }
 
-            // float test = (mesh.bounds.min).z + ((mesh.bounds.min).z - 0.5f);
-
             // BACK
-            if(verts[i].z == ( - halfSizePerTile.z))
+            if(verts[i].z == (-halfSizePerTile.z))
             {
                 Vector3 vert = verts[i];
                 vert.z = 0;
@@ -173,7 +183,7 @@ public class MeshModuleGenerator : Generator
         return module;
     }
 
-    private int Vector3Sort(Vector3 vert1, Vector3 vert2)
+    private static int Vector3Sort(Vector3 vert1, Vector3 vert2)
     {
         if(vert1.x == vert2.x)
         {
@@ -213,42 +223,136 @@ public class MeshModuleGenerator : Generator
         return 0;
     }
 
-    private void VisualDebug(Module[] modules)
-    {
-        for (int i = 0; i < modules.Length; i++)
-        {
-            for (int j = 0; j < modules[i].forwardNeighbors.Count; j++)
-            {
-                GameObject go = new GameObject();
-                go.AddComponent<MeshFilter>().mesh = modules[i].mesh;
-                go.AddComponent<MeshRenderer>().material = defaultMaterial;
-                go.transform.position = new Vector3(j, 0, i * 3);
-                go.name = modules[i].name;
+    // private void VisualDebug(Module[] modules)
+    // {
+    //     for (int i = 0; i < modules.Length; i++)
+    //     {
+    //         for (int j = 0; j < modules[i].forwardNeighbors.Count; j++)
+    //         {
+    //             GameObject go = new GameObject();
+    //             go.AddComponent<MeshFilter>().mesh = modules[i].mesh;
+    //             go.AddComponent<MeshRenderer>().material = defaultMaterial;
+    //             go.transform.position = new Vector3(j, 0, i * 3);
+    //             go.name = modules[i].name;
 
-                GameObject go2 = new GameObject();
-                go2.AddComponent<MeshFilter>().mesh = modules[i].forwardNeighbors[j].mesh;
-                go2.AddComponent<MeshRenderer>().material = defaultMaterial;
-                go2.transform.position = new Vector3(j, 0, 1 + i * 3);
-                go2.name = modules[i].forwardNeighbors[j].name;
+    //             GameObject go2 = new GameObject();
+    //             go2.AddComponent<MeshFilter>().mesh = modules[i].forwardNeighbors[j].mesh;
+    //             go2.AddComponent<MeshRenderer>().material = defaultMaterial;
+    //             go2.transform.position = new Vector3(j, 0, 1 + i * 3);
+    //             go2.name = modules[i].forwardNeighbors[j].name;
+    //         }
+    //     }
+    // }
+
+    private class Edge
+    {
+        public Vector3 first, second;
+        public Edge(Vector3 first, Vector3 second)
+        {
+            if(Vector3Sort(first, second) == 1)
+            {
+                this.first = second;
+                this.second = first;
+            }
+            else
+            {
+                this.first = first;
+                this.second = second;
             }
         }
+
+        public bool Compare(Edge other)
+        {
+            return (first == other.first && second == other.second);
+        }
+    }
+
+    private List<Edge> GetEdges(Mesh mesh)
+    {
+        List<Edge> edges = new List<Edge>(mesh.triangles.Length / 3);
+
+        for (int i = 0; i < mesh.triangles.Length - 2; i += 3)
+        {
+            {
+                Vector3 vert1 = mesh.vertices[mesh.triangles[i]];
+                Vector3 vert2 = mesh.vertices[mesh.triangles[i + 1]];
+                edges.Add(new Edge(vert1, vert2));
+            }
+            {
+                Vector3 vert1 = mesh.vertices[mesh.triangles[i + 1]];
+                Vector3 vert2 = mesh.vertices[mesh.triangles[i + 2]];
+                edges.Add(new Edge(vert1, vert2));
+            }
+            {
+                Vector3 vert1 = mesh.vertices[mesh.triangles[i + 2]];
+                Vector3 vert2 = mesh.vertices[mesh.triangles[i]];
+                edges.Add(new Edge(vert1, vert2));
+            }
+        }
+
+        edges.Sort(delegate(Edge edge1, Edge edge2)
+        {
+            int firstSort = Vector3Sort(edge1.first, edge2.first);
+            if(edge1.first == edge2.first)
+            {
+                return Vector3Sort(edge1.second, edge2.second);
+            }
+            else
+            {
+                return firstSort;
+            }
+        });
+
+        for (int i = edges.Count - 1; i >= 1 ; i--)
+        {
+            if(edges[i].Compare(edges[i - 1]) == true)
+            {
+                edges.RemoveAt(i);
+                edges.RemoveAt(i - 1);
+                i -= 1;
+            }
+        }
+
+        return edges;
+    }
+
+    private List<Vector3> GetVertsAlogEdges(Mesh mesh)
+    {
+        List<Edge> edges = GetEdges(mesh);
+        List<Vector3> edgeVerts = new List<Vector3>(edges.Count * 2);
+        for (int i = 0; i < edges.Count; i++)
+        {
+            edgeVerts.Add(edges[i].first);
+            edgeVerts.Add(edges[i].second);
+        }
+        return edgeVerts;
     }
 
     private void OnDrawGizmosSelected()
     {
+        // for (int j = 0; j < meshes.Length; j++)
+        // {
+        //     Vector3 position = transform.position + new Vector3(1 * j * sizePerTile.x, 0, 0);
+        //     Gizmos.color = Color.white;
+        //     Gizmos.DrawWireMesh(meshes[j], position, Quaternion.identity);
+        //     Gizmos.color = Color.blue;
+        //     Gizmos.DrawWireCube(position, sizePerTile);
+
+        //     // List<Vector3> verts = CreateModule(meshes[j]);
+        //     // for (int i = 0; i < verts.Count; i++)
+        //     // {
+        //     //     Gizmos.DrawSphere(verts[i] + position, .05f);
+        //     // }
+        // }
+
+        Vector3 offset = new Vector3(0, 0, -5);
         for (int j = 0; j < meshes.Length; j++)
         {
-            Vector3 position = transform.position + new Vector3(1 * j * sizePerTile.x, 0, 0);
-            Gizmos.color = Color.white;
-            Gizmos.DrawWireMesh(meshes[j], position, Quaternion.identity);
-            Gizmos.color = Color.blue;
-            Gizmos.DrawWireCube(position, sizePerTile);
-
-            // List<Vector3> verts = CreateModule(meshes[j]);
-            // for (int i = 0; i < verts.Count; i++)
-            // {
-            //     Gizmos.DrawSphere(verts[i] + position, .05f);
-            // }
+            List<Edge> edges = GetEdges(meshes[j]);
+            for (int i = 0; i < edges.Count; i++)
+            {
+                Gizmos.DrawLine(edges[i].first + offset + Vector3.right * (j + j), edges[i].second + offset + Vector3.right * (j + j));
+            }      
         }
     }
 }
